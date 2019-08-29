f9ceb21629c5cadca813f6defb6f0d567d14d500
Anshuman Verma <anshu.av97@gmail.com>
2019-08-29 12:58:41 +0530

    Fix post routes, create pages as per slugs

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,85 +13,33 @@ exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
   const postTemplate = require.resolve('./src/templates/post.jsx')
-  const categoryTemplate = require.resolve('./src/templates/category.jsx')
 
   const result = await wrapper(
     graphql(`
       {
         allPrismicBlogPost {
-          edges {
-            node {
-              id
-              uid
-              data {
-                blog_image {
-                  url
-                  alt
-                  copyright
-                }
-                body {
-                  html
-                }
-                title {
-                  text
-                }
-              }
-            }
-          }
-        }
-        categories: allPrismicCategory {
-          edges {
-            node {
-              data {
-                uid {
-                  text
-                }
-                title {
-                  text
-                }
-                description {
-                  text
-                }
-              }
-            }
+          nodes {
+            slugs
           }
         }
       }
     `)
   )
 
-  const categorySet = new Set()
-  console.log(result)
-  const postsList = result.data.allPrismicBlogPost.edges
+  console.log(result.data.allPrismicBlogPost.nodes[0].slugs)
+  const postsList = result.data.allPrismicBlogPost.nodes[0].slugs
 
   // Double check that the post has a category assigned
   postsList.forEach(edge => {
     console.log(edge)
-    if (edge.node.data.categories.category.tag.text) {
-      edge.node.data.categories.forEach(cat => {
-        categorySet.add(cat.category.document[0].data.name)
-      })
-    }
 
     // The uid you assigned in Prismic is the slug!
     createPage({
-      path: `/${edge.node.uid}`,
+      path: `/${edge}`,
       component: postTemplate,
       context: {
         // Pass the unique ID (uid) through context so the template can filter by it
-        uid: edge.node.uid,
-      },
-    })
-  })
-
-  const categoryList = Array.from(categorySet)
-
-  categoryList.forEach(category => {
-    createPage({
-      path: `/categories/${_.kebabCase(category)}`,
-      component: categoryTemplate,
-      context: {
-        category,
+        slug: edge,
       },
     })
   })
