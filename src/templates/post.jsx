31d7d0732b1b29b28baec2b1450f93b7327973b8
Anshuman Verma <anshu.av97@gmail.com>
2019-08-29 13:00:34 +0530

    render blog content on blog routes, some cleanup

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -2,58 +2,49 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import styled from '@emotion/styled'
-import { Layout, Listing, Wrapper, SliceZone, Title, SEO, Header } from '../components'
+import { Layout, Listing, Wrapper, SliceZone, Title, SEO } from '../components'
 import Categories from '../components/Listing/Categories'
 import website from '../../config/website'
 
-const Hero = styled.header`
-  background-color: ${props => props.theme.colors.greyLight};
-  padding-top: 1rem;
-  padding-bottom: 4rem;
-`
-
-const Headline = styled.p`
-  font-family: 'Source Sans Pro', -apple-system, 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica', 'Arial',
-    sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-  color: ${props => props.theme.colors.grey};
-  font-size: 1.25rem;
-  a {
-    font-style: normal;
-    font-weight: normal;
-  }
-`
-
 const PostWrapper = Wrapper.withComponent('main')
 
-const Post = ({ data: { prismicPost, posts }, location }) => {
-  const { data } = prismicPost
-  let categories = false
-  if (data.categories[0].category) {
-    categories = data.categories.map(c => c.category.document[0].data.name)
-  }
+const BlogInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+`
+
+const renderContent = html => {
+  return {__html: html}
+}
+
+const Post = ({ data: { prismicPost }, location }) => {
+  console.log(prismicPost.data)
+  const { author, blog_image, body, published_on, title } = prismicPost.data
+  const { author_image, author_name, author_position } = author.document[0].data
   return (
     <Layout customSEO>
       <SEO
-        title={`${data.title.text} | ${website.titleAlt}`}
+        title={`${title.text} | ${website.titleAlt}`}
         pathname={location.pathname}
-        desc={data.description}
+        desc={title.text}
         node={prismicPost}
         article
       />
-      <Hero>
-        <Wrapper>
-          <Header />
-          <Headline>
-            {data.date} — {categories && <Categories categories={categories} />}
-          </Headline>
-          <h1>{data.title.text}</h1>
-        </Wrapper>
-      </Hero>
-      <PostWrapper id={website.skipNavId}>
+      <PostWrapper>
+        <img src={blog_image.url} alt={blog_image.alt} />
+        <BlogInfoWrapper>
+          <h1>{title.text}</h1>
+          <img src={author_image.url} alt={author_name.text} />
+          <span>{`${author_name.text}, ${author_position.text}`}</span>
+          <div dangerouslySetInnerHTML={{__html: body.html}} />
+        </BlogInfoWrapper>
+      </PostWrapper>
+      {/* <PostWrapper id={website.skipNavId}>
         <SliceZone allSlices={data.body} />
         <Title style={{ marginTop: '4rem' }}>Recent posts</Title>
         <Listing posts={posts.nodes} />
-      </PostWrapper>
+      </PostWrapper> */}
     </Layout>
   )
 }
@@ -74,18 +65,35 @@ Post.propTypes = {
 // If this doesn't work for you query for __typename in body {} and GraphiQL will show them to you
 
 export const pageQuery = graphql`
-  query BlogPostBySlug($uid: String!) {
-    prismicBlogPost(uid: { eq: $uid }) {
-      id
+  query BlogPostBySlug($slug: String!) {
+    prismicPost: prismicBlogPost(slugs: { eq: $slug }) {
       data {
+        published_on
         title {
           text
         }
-        blog_image {
-          url
-        }
         body {
           html
+        }
+        blog_image {
+          alt
+          url
+        }
+        author {
+          document {
+            data {
+              author_image {
+                alt
+                url
+              }
+              author_name {
+                text
+              }
+              author_position {
+                text
+              }
+            }
+          }
         }
       }
     }
