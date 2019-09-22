e61b70ecf6d522d5177bc1a49e56fb0fa471fe47
Anshuman Verma <anshu.av97@gmail.com>
2019-09-23 00:16:50 +0530

    add gatsby-plugin-robots for better SEO

--- config/website.js
+++ config/website.js
@@ -18,7 +18,7 @@ module.exports = {
 
   twitter: '@clarisights', // Twitter Username
   facebook: 'ClarisightsOfficial', // Facebook Site Name
-  googleAnalyticsID: '',
+  googleAnalyticsID: 'UA-148226838-1',
 
   skipNavId: 'reach-skip-nav', // ID for the "Skip to content" a11y feature
 }
