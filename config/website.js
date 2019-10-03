49a0a719933a096d0bfa1d5e43ff0a8a49312cbd
Srijan Agarwal <srijanagarwal.cse@gmail.com>
2019-10-03 17:02:39 +0530

    chore: update description (#31)

--- config/website.js
+++ config/website.js
@@ -2,8 +2,8 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
   title: 'Clarisights Journal', // Navigation and Site Title
   titleAlt: 'Clarisights Journal', // Title for JSONLD
-  description: 'Where throughts from clarisights come alive',
-  headline: 'Clarisights - The decision platform for growth', // Headline for schema.org JSONLD
+  description: 'Where thoughts from clarisights come alive',
+  headline: 'Clarisights - The next gen BI platform for data driven marketing teams', // Headline for schema.org JSONLD
   url: 'https://clarisights-journal.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   logo:
